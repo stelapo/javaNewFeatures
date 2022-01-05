755b5788a93653360ce0d38572198a26bf84af33
Lapo Pancani <lapo.pancani@3esse.retexspa.com>
2022-01-05 11:09:00 +0100

    Aggiunta classe java11

--- src/test/Java11.java
+++ src/test/Java11.java
@@ -2,6 +2,13 @@ package test;
 
 import test.util.Print;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.stream.Collectors;
+
 import static test.util.Print.p;
 
 public class Java11 {
@@ -14,7 +21,28 @@ public class Java11 {
         multilineStr.lines().forEach(Print::p);
     }
 
-    public static void main(String[] args) {
+    private static void testHttpClient() throws IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newBuilder().build();
+
+        HttpRequest request =
+                HttpRequest.newBuilder()
+                        .version(HttpClient.Version.HTTP_2)
+                        .uri(URI.create("https://advancedweb.hu/"))
+                        .GET()
+                        .build();
+
+        HttpResponse<String> response =
+                httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Print.p("response.statusCode()=" + response.statusCode());
+        Print.p("response.body()=" + response.body());
+        response.headers().map().forEach((s, strings) -> {
+            Print.p(s + "=" + strings.stream().collect(Collectors.joining(",")));
+        });
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         testLines();
+        testHttpClient();
     }
 }
